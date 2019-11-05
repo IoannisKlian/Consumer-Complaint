312d429faefc028964936914a65e91ff63f2ee35
Ioannis Klian <klian12@yahoo.com>
2019-11-05 12:59:18 +0200

    changed db file

--- DB/consumer_complaint.sql
+++ DB/consumer_complaint.sql
@@ -1,17 +1,45 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Φιλοξενητής: 127.0.0.1
+-- Χρόνος δημιουργίας: 05 Νοε 2019 στις 11:54:49
+-- Έκδοση διακομιστή: 10.1.38-MariaDB
+-- Έκδοση PHP: 7.3.2
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Βάση δεδομένων: `consumer_complaint`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Δομή πίνακα για τον πίνακα `complaint`
+--
 
 CREATE TABLE `complaint` (
   `id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `email` varchar(20) NOT NULL,
-  `phonenumber` varchar(50) NOT NULL,
-  `company_name` varchar(100) NOT NULL,
-  `company_adress` varchar(50) NOT NULL,
-  `company_phone` varchar(50) NOT NULL,
-  `company_taxid` varchar(50) NOT NULL,
-  `description` varchar(5000) NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(20) CHARACTER SET latin1 NOT NULL,
+  `phonenumber` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `company_name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `company_adress` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `company_phone` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `company_taxid` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `description` varchar(5000) COLLATE utf8mb4_unicode_ci NOT NULL,
   `datetime` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -21,7 +49,7 @@ CREATE TABLE `complaint` (
 
 CREATE TABLE `file` (
   `id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `complaint_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -37,13 +65,6 @@ CREATE TABLE `govrn_emp` (
   `password` varchar(30) NOT NULL,
   `name` varchar(30) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Άδειασμα δεδομένων του πίνακα `govrn_emp`
---
-
-INSERT INTO `govrn_emp` (`id`, `username`, `password`, `name`) VALUES
-(1, 'boredemp1', 'password', 'papadopoulosioannis');
 
 --
 -- Ευρετήρια για άχρηστους πίνακες
@@ -76,7 +97,7 @@ ALTER TABLE `govrn_emp`
 -- AUTO_INCREMENT για πίνακα `complaint`
 --
 ALTER TABLE `complaint`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
 
 --
 -- AUTO_INCREMENT για πίνακα `file`
@@ -88,7 +109,7 @@ ALTER TABLE `file`
 -- AUTO_INCREMENT για πίνακα `govrn_emp`
 --
 ALTER TABLE `govrn_emp`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
 
 --
 -- Περιορισμοί για άχρηστους πίνακες
@@ -101,4 +122,6 @@ ALTER TABLE `file`
   ADD CONSTRAINT `foreign_key_complaint_id` FOREIGN KEY (`complaint_id`) REFERENCES `complaint` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
-
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
