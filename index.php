e3967a3a392fcac5b18351388647102199f52428
dimpan10 <dimpan10@hotmail.com>
2019-11-05 13:06:10 +0200

    new ui

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
 </head>
 
 <body>
+<div style="padding-left:0.75%;">
 
 <form action="complaintSumbitHandler.php" id="form" method="post" enctype="multipart/form-data">
 
@@ -51,31 +52,61 @@
   </div>
 
 
-  <input type="text" placeholder="Επωνυμία εταιρείας" id="company_name" name="company_name" required>
+  <div class="form-row">
+    <div class="col-md-4 mb-3">
+      <label for="company_name">Επωνυμία εταιρείας*</label>
+      <input type="text" class="form-control" id="company_name" placeholder="Επωνυμία εταιρείας" name="company_name" required>
+      <div class="invalid-tooltip">
+        Please provide a valid city.
+      </div>
+    </div>
 
-  <span class="error">*</span><br>
-  <input type="text" placeholder="Διεύθυνση εταιρείας" id="company_address" name="company_address">
-  <span class="error">*</span><br>
-  <input type="numbers" placeholder="Τηλέφωνο επικοινωνίας εταιρείας" id="company_phone" name="company_phone">
-  <span class="error">*</span><br>
-  <input type="numbers" placeholder="ΑΦΜ εταιρείας" id="company_taxId" name="company_taxId">
-  <span class="error">*</span><br><br>
+    <div class="col-md-4 mb-3">
+      <label for="company_address">Διεύθυνση εταιρείας*</label>
+      <input type="text" class="form-control" id="company_address" placeholder="Διεύθυνση εταιρείας" name="company_address" required>
+      <div class="invalid-tooltip">
+        Please provide a valid address.
+      </div>
+    </div>
+  </div>
 
+  <div class="form-row">
+    <div class="col-md-4 mb-3">
+      <label for="company_taxId">ΑΦΜ εταιρείας</label>
+      <input type="numbers" class="form-control" id="company_taxId" placeholder="ΑΦΜ" name="company_taxId">
+    </div>
+ 
+    <div class="col-md-4 mb-3">
+      <label for="company_phone">Τηλέφωνο επικοινωνίας εταιρείας</label>
+      <input type="numbers" class="form-control" id="company_phone" placeholder="Τηλέφωνο" name="company_phone">
+    </div>
+  </div>
+
+<div style="padding-right:33.6%;">
   <div class="input-group">
     <div class="input-group-prepend">
     <span class="input-group-text">Περιγραφή προβλήματος</span>
   </div>
   <textarea class="form-control" aria-label="Περιγραφή προβλήματος" id="comment" name="comment" placeholder="text" required></textarea>
   </div>
-  
-  <br><br> 
+</div><br> 
 
-  Evidence file(image,audio etc.): Select image to upload:
-    <br>
-  <input type="file" name="file" id="file"><br>
+<div style="padding-right:33.6%;">
+<div class="input-group">
+  <div class="input-group-prepend">
+    <span class="input-group-text" id="file">Αποδεικτικά στοιχεία</span>
+  </div>
+  <div class="custom-file">
+    <input type="file" class="custom-file-input" id="file"
+      aria-describedby="inputGroupFileAddon01">
+    <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+  </div>
+</div>
+</div><br>
 
-  <input type="button" id="submit-btn" name="submit-btn" value="Submit">
+  <button type="button" class="btn btn-secondary" value="Submit" id="submit-btn" >Submit</button>
 
 </form>
+</div>
 </body>
 </HTML>
