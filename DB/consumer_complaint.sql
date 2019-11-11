e94d99be1538b115b019b9506b3edbcd7c267985
Alexandros Kanakis <alkanakis@windowslive.com>
2019-11-11 11:03:22 +0200

    update DB

--- DB/consumer_complaint.sql
+++ DB/consumer_complaint.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.7.9
 -- https://www.phpmyadmin.net/
 --
--- Φιλοξενητής: 127.0.0.1
--- Χρόνος δημιουργίας: 05 Νοε 2019 στις 11:54:49
--- Έκδοση διακομιστή: 10.1.38-MariaDB
--- Έκδοση PHP: 7.3.2
+-- Host: 127.0.0.1:3306
+-- Generation Time: Nov 11, 2019 at 09:02 AM
+-- Server version: 5.7.21
+-- PHP Version: 7.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,107 +19,50 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Βάση δεδομένων: `consumer_complaint`
+-- Database: `consumer_complaint`
 --
 
 -- --------------------------------------------------------
 
 --
--- Δομή πίνακα για τον πίνακα `complaint`
+-- Table structure for table `complaint`
 --
 
-CREATE TABLE `complaint` (
-  `id` int(11) NOT NULL,
-  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(20) CHARACTER SET latin1 NOT NULL,
-  `phonenumber` varchar(50) CHARACTER SET latin1 NOT NULL,
-  `company_name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `company_adress` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `company_phone` varchar(50) CHARACTER SET latin1 NOT NULL,
-  `company_taxid` varchar(50) CHARACTER SET latin1 NOT NULL,
-  `description` varchar(5000) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `datetime` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Δομή πίνακα για τον πίνακα `file`
---
-
-CREATE TABLE `file` (
-  `id` int(11) NOT NULL,
-  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `complaint_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `complaint`;
+CREATE TABLE IF NOT EXISTS `complaint` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `phonenumber` varchar(50) NOT NULL,
+  `company_name` varchar(100) NOT NULL,
+  `company_adress` varchar(50) NOT NULL,
+  `company_phone` varchar(50) NOT NULL,
+  `company_taxid` varchar(50) NOT NULL,
+  `description` varchar(5000) NOT NULL,
+  `datetime` datetime NOT NULL,
+  `status` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=latin1;
 
 --
--- Δομή πίνακα για τον πίνακα `govrn_emp`
+-- Dumping data for table `complaint`
 --
 
-CREATE TABLE `govrn_emp` (
-  `id` int(10) NOT NULL,
-  `username` varchar(30) NOT NULL,
-  `password` varchar(30) NOT NULL,
-  `name` varchar(30) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Ευρετήρια για άχρηστους πίνακες
---
-
---
--- Ευρετήρια για πίνακα `complaint`
---
-ALTER TABLE `complaint`
-  ADD PRIMARY KEY (`id`);
-
---
--- Ευρετήρια για πίνακα `file`
---
-ALTER TABLE `file`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `foreign_key_complaint_id` (`complaint_id`);
-
---
--- Ευρετήρια για πίνακα `govrn_emp`
---
-ALTER TABLE `govrn_emp`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT για άχρηστους πίνακες
---
-
---
--- AUTO_INCREMENT για πίνακα `complaint`
---
-ALTER TABLE `complaint`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
-
---
--- AUTO_INCREMENT για πίνακα `file`
---
-ALTER TABLE `file`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT για πίνακα `govrn_emp`
---
-ALTER TABLE `govrn_emp`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
-
---
--- Περιορισμοί για άχρηστους πίνακες
---
-
---
--- Περιορισμοί για πίνακα `file`
---
-ALTER TABLE `file`
-  ADD CONSTRAINT `foreign_key_complaint_id` FOREIGN KEY (`complaint_id`) REFERENCES `complaint` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+INSERT INTO `complaint` (`id`, `name`, `email`, `phonenumber`, `company_name`, `company_adress`, `company_phone`, `company_taxid`, `description`, `datetime`, `status`) VALUES
+(40, 'a a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', '2019-10-31 17:01:44', 0),
+(41, 'a a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', '2019-10-31 17:03:36', 0),
+(42, 's ss', 's', 's', 's', 's', 's', 's', 's', '2019-10-31 17:05:46', 0),
+(43, 'd d', 'd', 'd', 'd', 'd', 'd', 'd', 'd', '2019-10-31 17:06:38', 0),
+(44, 'y y', 'y', 'y', 'y', 'y', 'y', 'y', 'y', '2019-10-31 17:11:24', 0),
+(45, 'y y', 'y', 'y', 'y', 'y', 'y', 'y', 'y', '2019-10-31 17:12:43', 0),
+(46, 'j j', 'j', 'j', 'j', 'j', 'j', 'j', 'j', '2019-10-31 17:24:58', 0),
+(47, 'j j', 'j', 'j', 'j', 'j', 'j', 'j', 'j', '2019-10-31 17:26:33', 0),
+(48, 'l l', 'l', 'l', 'l', 'l', 'l', 'l', 'l', '2019-10-31 17:29:47', 0),
+(49, 'g g', 'g', 'g', 'g', 'g', 'g', 'g', 's', '2019-11-04 14:06:44', 0),
+(50, 'empty', 'g', 'g', 'g', 'g', 'g', 'g', 'g', '2019-11-04 14:12:46', 0),
+(51, 'h h', 'empty', 'ws', 'aaaa', 'a', 'a', 'a', 'a', '2019-11-04 14:51:40', 0),
+(52, 'd d', 'd', 'd', 'd', 'd', 'dd', 'd', 'd', '2019-11-04 14:51:52', 0),
+(53, 'j j', 'j', 'j', 'j', 'j', 'j', 'j', 'jj', '2019-11-11 10:59:37', 0);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
