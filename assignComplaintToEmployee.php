bb40331cdf48f7738c7391a5c4216d84bea9253a
Ioannis Klian <klian12@yahoo.com>
2019-11-12 15:05:01 +0200

    updated

---
+++ assignComplaintToEmployee.php
@@ -0,0 +1,17 @@
+<?php
+	session_start();
+	if (!isset($_SESSION['"id"']) && !isset($_SESSION['complaintID'])) {
+		header("Location: login.php");
+	}
+	else{
+		$userID=$_SESSION['"id"'];
+		$complaintID = $_SESSION['complaintID'];
+	}
+
+
+	include ("connect.php");
+
+	mysqli_query($connection,"INSERT INTO `employee_complaint` (`id`, `employee_id`, `complaint_id`, `datetime`) VALUES (DEFAULT,'".$userID."', '".$complaintID."', now())");
+
+
+?>
