e4779c7752a2031ca8ed5558bae890e9ff26fa23
dimpan10 <dimpan10@hotmail.com>
2019-11-19 16:16:07 +0200

    r

--- employee/employee_nav.php
+++ employee/employee_nav.php
@@ -1,21 +1,21 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#"></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav nav-fill w-100">
       <li class="nav-item active" id="index" style="border-right: solid;  border-color: #a1f8ff;">
         <a class="nav-link" href="employeeIndex.php">Ανοιχτά<span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item dropdown" id="pending" style="border-right: solid; border-color: #a1f8ff;">
-       <a class="nav-link dropdown-toggle" href="employeePending.php" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <a class="nav-link dropdown-toggle" href="employeePending.php" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Εκκρεμή
         </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-        </div>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+          </div>
       </li>
       <li class="nav-item">
         <a class="nav-link" id="archived" href="employeeArchived.php">Αρχειοθετημένα</a>
@@ -29,13 +29,4 @@
   </div>
 </nav>
 
-<!--<li class="nav-item dropdown" id="pending" style="border-right: solid; border-color: #a1f8ff;">
-       <a class="nav-link dropdown-toggle" href="employeePending.php" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Εκκρεμή
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-        </div>
-      </li>
-      -->
+        
