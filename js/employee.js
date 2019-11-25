9d4732b79a97f1da332cce990f25d28a59d2b251
Alexandros Kanakis <alkanakis@windowslive.com>
2019-11-25 12:17:32 +0200

    updated navbar activation colors

--- js/employee.js
+++ js/employee.js
@@ -2,23 +2,24 @@ $( document ).ready(function() {
 	
 	//Shameful way to handle active navbar buttons, since Bootstrap decided be a little bitch
 	var pageName = document.location.href.match(/[^\/]+$/)[0];
-	switch(pageName) {
-		case "employeeIndex.php":
-		$("#employee-open").css('color', 'black');
-		$("#employee-pending").css('color', 'gray');
-		$("#employee-archived").css('color', 'gray');
-		break;
-		case "employeePending.php":
-		$("#employee-open").css('color', 'gray');
-		$("#employee-pending").css('color', 'black');
-		$("#employee-archived").css('color', 'gray');
-		break;
-		case "employeeArchived.php":
-		$("#employee-open").css('color', 'gray');
-		$("#employee-pending").css('color', 'gray');
-		$("#employee-archived").css('color', 'black');
-		break;
-	}
+		console.log(pageName)
+
+		if (pageName.includes("employeeIndex")) {
+			$("#employee-open").css('color', 'black');
+			$("#employee-pending").css('color', 'gray');
+			$("#employee-archived").css('color', 'gray');
+		}
+		else if (pageName.includes("employeeIndex")) {
+			$("#employee-open").css('color', 'gray');
+			$("#employee-pending").css('color', 'black');
+			$("#employee-archived").css('color', 'gray');
+		}
+		else if (pageName.includes("employeeIndex")) {
+			$("#employee-open").css('color', 'gray');
+			$("#employee-pending").css('color', 'gray');
+			$("#employee-archived").css('color', 'black');
+		}
+
 });
 
 
