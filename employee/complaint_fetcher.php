a5efe92e257f00eecc354a7a9cddcf6011a88251
dimpan10 <dimpan10@hotmail.com>
2019-11-25 12:31:34 +0200

    complait fetcher new table

--- employee/complaint_fetcher.php
+++ employee/complaint_fetcher.php
@@ -1,22 +1,41 @@
-<?php while($row = mysqli_fetch_array($res_data)){
+<?php 
+    echo '<table class="table table-striped">
+    <thead class="thead-dark">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Ονομασία επιχείρησης</th>
+      <th scope="col">Όνομα καταγγελέα</th>
+      <th scope="col">Ημερομηνία καταβολής</th>
+      <th scope="col"></th>
+    </tr>
+  </thead>
+  <tbody>';
 
-    echo "<div class="."container".">";
-    echo "<h3>"."Ονομασία επιχείρησης: ".$row['company_name']."</h3>";
+  $counter = 0;
+
+    while($row = mysqli_fetch_array($res_data)){
+        $counter++;
     if (strcmp( "empty", $row['name'] ) == 0) {
       $nameOfComplainer = "-";
     }
     else{
       $nameOfComplainer = $row['name'];
     }
-    echo "<h5>"."Όνομα καταγγελέα: ".$nameOfComplainer."</h5>";
-    echo "<h5>"."Ημερομηνία καταβολής: ".$row['datetime']."</h5>";
-    echo '<form action="complaint_box.php" method="post">';
-    echo '<input type="submit" name="Άνοιγμα καταγγελίας" value="Άνοιγμα καταγγελίας" class="btn btn-secondary" formtarget="_blank" />';
-    echo '<input type="hidden" id="complaintID" name="complaintID" value="'.$row['id'].'">';
-	echo "</form>";
-    echo '</div> <hr style="border-width: 2px;color: black;">';
+   echo' <tr>
+      <th scope="row">'.$counter.'</th>
+      <td>'.$row["company_name"].'</td>
+      <td>'.$nameOfComplainer.'</td>
+      <td>'.$row['datetime'].'</td>
+      <td><form action="complaint_box.php" method="post">
+        <input type="submit" name="Άνοιγμα καταγγελίας" value="Άνοιγμα καταγγελίας" class="btn btn-secondary" formtarget="_blank" />
+        <input type="hidden" id="complaintID" name="complaintID" value="'.$row['id'].'">
+        </form>
+      </td>
+    </tr>';
+    
 }
-
+echo '</tbody>
+</table>';
 mysqli_close($connection);
 
 ?>
