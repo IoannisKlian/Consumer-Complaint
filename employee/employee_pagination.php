0a671863eb025ea56565cf9f8cb6ab425b881d4b
Ioannis Klian <klian12@yahoo.com>
2019-11-26 15:44:02 +0200

    some fixes

--- employee/employee_pagination.php
+++ employee/employee_pagination.php
@@ -1,33 +1,17 @@
-<ul class="pagination" style="position: absolute; margin-top: 10%; left: 60%;">
-    <li><a href="?pageno=1">First</a></li>
-    <li class="<?php if($pageno <= 1){ echo 'disabled'; } ?>">
-        <a href="<?php if($pageno <= 1){ echo '#'; } else { echo "?pageno=".($pageno - 1); } ?>">Prev</a>
+<ul class="pagination" >
+    <li><a class="page-link" href="?pageno=1">First</a></li>
+    <li class="<?php if($pageno <= 1){ echo 'page-item disabled'; } ?>">
+        <a class="page-link" href="<?php if($pageno <= 1){ echo '#'; } else { echo "?pageno=".($pageno - 1); } ?>">Prev</a>
     </li>
     <?php
       for ($i = 1; $i <= $total_pages; $i++) {
-      echo "<li class=";
-      if($pageno == $i){ echo 'disabled'; }
-      echo "><a href="."?pageno=".$i.">".$i."</a></li>";
-  } 
+        echo "<li class=";
+        if($pageno == $i){ echo '"'.'page-item disabled'.'"'; }
+        echo '><a class="page-link" href="?pageno='.$i.'">'.$i.'</a></li>';
+      } 
     ?>
-    <li class="<?php if($pageno >= $total_pages){ echo 'disabled'; } ?>">
-        <a href="<?php if($pageno >= $total_pages){ echo '#'; } else { echo "?pageno=".($pageno + 1); } ?>">Next</a>
+    <li class="<?php if($pageno >= $total_pages){ echo 'page-item disabled'; } ?>">
+        <a class="page-link" href="<?php if($pageno >= $total_pages){ echo '#'; } else { echo "?pageno=".($pageno + 1); } ?>">Next</a>
     </li>
-    <li><a href="?pageno=<?php echo $total_pages; ?>">Last</a></li>
+    <li><a class="page-link" href="?pageno=<?php echo $total_pages; ?>">Last</a></li>
 </ul>
-
-<!-- <nav aria-label="...">
-  <ul class="pagination">
-    <li class="page-item disabled">
-      <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
-    </li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item active" aria-current="page">
-      <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
-    </li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item">
-      <a class="page-link" href="#">Next</a>
-    </li>
-  </ul>
-</nav> -->
