7abc1b050d39ece4dda6a742069d0e0ff45770e5
Alexandros Kanakis <alkanakis@windowslive.com>
2019-11-28 13:36:18 +0200

    fix minor sorting issue

--- employee/complaint_table.php
+++ employee/complaint_table.php
@@ -6,6 +6,8 @@
       <th scope="col">Όνομα καταγγελέα</th>
       <th scope="col">Ημερομηνία καταβολής</th>
       <th scope="col">Αναλήφθηκε απο</th>
+
+      <!-- Sorting button -->
       <th scope="col" style="text-align: right;">
         <button class="nav-link dropdown-toggle btn btn-dark" id="sort_button" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="padding: 0; text-align: right; float: right;">
           Ταξινόμηση
@@ -13,13 +15,14 @@
         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
           <?php 
           echo '<a class="dropdown-item" href="employee_index.php?page_id='.$_SESSION['pageID'].'&sort_by=id ASC">Νεότερα</a>
-                <a class="dropdown-item" href="employee_index.php?page_id='.$_SESSION['pageID'].'&sort_by=id ASC">Παλαιότερα</a>
+                <a class="dropdown-item" href="employee_index.php?page_id='.$_SESSION['pageID'].'&sort_by=id DESC">Παλαιότερα</a>
                 <a class="dropdown-item" href="employee_index.php?page_id='.$_SESSION['pageID'].'&sort_by=username ASC">Ανα Υπάλληλο (Αυξουσα Σειρά)
                 <a class="dropdown-item" href="employee_index.php?page_id='.$_SESSION['pageID'].'&sort_by=username DESC">Ανα Υπάλληλο (Φθήνουσα Σειρά)
                 </a>'; 
           ?>
-        </div></th>
-      <th scope="col"></th>
+        </div>
+      </th>
+      <!--  -->
     </tr>
   </thead>
   <tbody>
