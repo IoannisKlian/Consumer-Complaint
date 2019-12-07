4080e5002d0371a1a40dfcfb0c47266d23e8951d
Ioannis Klian <klian12@yahoo.com>
2019-12-07 11:42:56 +0200

    fixed authetication

--- employee/auth.php
+++ employee/auth.php
@@ -1,25 +1,36 @@
 <?php  
+	
 
 	include ("../connect.php");
 
-	$username = stripslashes($_POST['username']);
-	$password = stripslashes($_POST['password']);
+	if (isset($_POST['username'])&&isset($_POST['password'])) {
+		if (!empty($_POST['username']) && !empty($_POST['password'])) {
+			$username = stripslashes($_POST['username']);
+			$password = stripslashes($_POST['password']);
 
-	$username = mysqli_real_escape_string($connection, $username);
-	$password = mysqli_real_escape_string($connection, $password);
+			$username = mysqli_real_escape_string($connection, $username);
+			$password = mysqli_real_escape_string($connection, $password);
 
-	$result = mysqli_query($connection, "SELECT * FROM govrn_emp WHERE username = '$username' AND password = '$password'");
-	$row = mysqli_fetch_array($result);
-	
-	
-	if ($row['username'] == $username && $row['password'] == $password ) {
-		session_start();
-		$_SESSION["user"] = true;
-		$_SESSION['id'] = $row['id'];
-		$_SESSION["user_name"] = $row['username'];
-		$_SESSION["name"] = $row['name'];
-		
-		header("Location: complaint-list/");
+			$result = mysqli_query($connection, "SELECT * FROM govrn_emp WHERE username = '$username' AND password = '$password'");
+			$row = mysqli_fetch_array($result);
+			
+			
+			if ($row['username'] == $username && $row['password'] == $password ) {
+				echo $row['username'];
+				echo $username;
+				$_SESSION["user"] = true;
+				$_SESSION['id'] = $row['id'];
+				$_SESSION["user_name"] = $row['username'];
+				$_SESSION["name"] = $row['name'];
+				
+				header("Location: complaint-list/");
+			}
+			else {
+				session_start();
+				$_SESSION['errorinput'] = true;
+				header("Location: login.php");
+			}
+		}
 	}
 	else {
 		session_start();
