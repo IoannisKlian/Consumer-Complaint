dd9b0b04f066729f278153739d3afe6ab8cf8a0b
Alexandros Kanakis <alkanakis@windowslive.com>
2019-12-10 14:47:23 +0200

    removed java

--- employee/admin/admin_nav.php
+++ employee/admin/admin_nav.php
@@ -8,12 +8,12 @@ if (isset($_GET["admin_id"])) {
   }
 ?>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-dark" style="padding-bottom: 0; margin-bottom: 0; border-top: solid; padding-top: 0; margin-top: 0;">
-  <div class="collapse navbar-collapse" id="navbarNavDropdown1" style="padding-top: 0; margin-top: 0;">
-    <ul class="navbar-nav nav-fill w-100" style="padding-top: 0; margin-top: 0;">
+<nav class="navbar navbar-expand-lg navbar-light bg-dark" style="padding-bottom: 0; margin-bottom: 0; border-top: solid; padding-top: 0; margin-top: 0;  padding-left: 0; margin-left: 0;">
+  <div class="collapse navbar-collapse" id="navbarNavDropdown1" style="padding-top: 0; margin-top: 0;  padding-left: 0; margin-left: 0;">
+    <ul class="navbar-nav nav-fill w-100" style="padding-top: 0; margin-top: 0;  padding-left: 0; margin-left: 0;">
 
       <!-- Add User -->
-      <li class="nav-item" id="add-user" style="border-right: solid; padding-top: 0; margin-top: 0;">
+      <li class="nav-item" id="add-user" style="border-right: solid; padding-top: 0; margin-top: 0; padding-left: 0; margin-left: 0;">
         <a class="nav-link" style="color: white;"  href="?admin_id=0">Προσθήκη Χρήστη</a>
       </li>
 
