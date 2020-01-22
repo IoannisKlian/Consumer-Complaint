9f1126f4d300b8f0a89af8a3ffaa6f012a5b6eb7
Alexandros Kanakis <alkanakis@windowslive.com>
2020-01-22 14:13:41 +0200

    form responsiveness fix

--- form.php
+++ form.php
@@ -177,9 +177,9 @@
 </div>
 </div>
 
-<!-- <div id="map" style="height: 100%"></div>
+<!-- <div id="map" style="height: 100%"></div> -->
 
-    <script>
+<!--     <script>
       // Note: This example requires that you consent to location sharing when
       // prompted by your browser. If you see the error "The Geolocation service
       // failed.", it means you probably did not give permission for the browser to
