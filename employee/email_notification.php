163d667b8ce40d31838ff30150bb567fc12d972a
Ioannis Klian <klian12@yahoo.com>
2020-02-27 13:15:56 +0200

    g

--- employee/email_notification.php
+++ employee/email_notification.php
@@ -1,9 +1,9 @@
 <?php
-	/*
+	
 	$res_data = mysqli_query($connection,"SELECT * FROM govrn_emp ");
 	while($row = mysqli_fetch_array($res_data)){
 		$text_to_show = 'Μια νέα καταγγελία καταχωρήθηκε με αριθμό #'.$complaint_id;
 		$email = mail($row['email'],'=?UTF-8?B?' . base64_encode('Νέα καταγγελία!'). '?=', $text_to_show, "From:"." giannisklian@gmail.com");
 	}
-	*/	
+	
 ?>
