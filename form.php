2ca45b893a995c9b23f0abe01f9db42854c6b606
Alexandros Kanakis <alkanakis@windowslive.com>
2020-02-27 11:45:23 +0200

    fix title

--- form.php
+++ form.php
@@ -6,6 +6,7 @@
 
 <HTML>
 <head>
+  <title>Φόρμα Καταγγελίας Υπηρεσίας</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -48,7 +49,7 @@
 
   <!-- Title -->
   <div class="form-label" style="background-color: #e9ecef" > 
-    <div style="font-size: 3vh; color: #5b5d5f; padding-left:5%; padding-top: 1vh; padding-bottom: 1vh;"> Φόρμα Καταγγελείας Υπηρεσίας</div>
+    <div style="font-size: 3vh; color: #5b5d5f; padding-left:5%; padding-top: 1vh; padding-bottom: 1vh;"> Φόρμα Καταγγελίας Υπηρεσίας</div>
   </div>
 
   <!-- Main container -->
